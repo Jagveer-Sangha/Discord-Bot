6aeeb77f6b347d6ce6f844cbf17ad2e80aa3673d
Jagveer-Sangha <61700208+Jagveer-Sangha@users.noreply.github.com>
2021-05-03 18:20:37 -0400

    Update Constants.java

--- src/main/java/discord/bot/plug/Constants.java
+++ src/main/java/discord/bot/plug/Constants.java
@@ -2,7 +2,7 @@ package discord.bot.plug;
 
 public class Constants {
 	
-	static String TOKEN = "NzcxODg0MTQ1MTc3MjY0MTg4.X5ynIQ.evdY5XxPam32LYF0hTGYP3js2-E";
-	static String BOTSTATUS = "SMOLPPBEAN";
+	static String TOKEN = "";
+	static String BOTSTATUS = "";
 	
 }

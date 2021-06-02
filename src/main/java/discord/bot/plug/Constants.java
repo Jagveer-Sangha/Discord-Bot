2b5fce08c762117b331405ea3cde38e89be8d211
Jagveer Sangha <jagveersangha@gmail.com>
2021-06-01 23:46:58 -0400

    Updated Constants.java

--- src/main/java/discord/bot/plug/Constants.java
+++ src/main/java/discord/bot/plug/Constants.java
@@ -1,8 +1,15 @@
 package discord.bot.plug;
 
-public class Constants {
+//imports
+// import io.github.cdimascio.dotenv.Dotenv;
 
+public class Constants {
 	static String TOKEN = "";
 	static String BOTSTATUS = "Chillin";
+
+	// static String environmentvariables() {
+	// Dotenv dotenv = Dotenv.load();
+	// return dotenv.get("DISCORD_BOT_TOKEN");
+	// }
 
 }

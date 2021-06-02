67c0878874d23d15b245867c3f338fbfbde743dc
Jagveer Sangha <jagveersangha@gmail.com>
2021-06-02 09:27:17 -0400

    Updated Constants.java with the ability to safely retrieve an API key from an .env file

--- src/main/java/discord/bot/plug/Constants.java
+++ src/main/java/discord/bot/plug/Constants.java
@@ -1,15 +1,22 @@
 package discord.bot.plug;
 
 //imports
-// import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Constants {
-	static String TOKEN = "";
-	static String BOTSTATUS = "Chillin";
 
-	// static String environmentvariables() {
-	// Dotenv dotenv = Dotenv.load();
-	// return dotenv.get("DISCORD_BOT_TOKEN");
-	// }
+	/**
+	 * Instantiates a new Constants. Private to prevent instantiation
+	 */
+	private Constants() {
+		// not called
+	}
+
+	// Gets access and loads a .env file
+	static Dotenv dotenv = Dotenv.configure().filename(".env").load();
+	// Retrieves API key from .env file
+	static String TOKEN = dotenv.get("DISCORD_BOT_TOKEN");
+	
+	static String BOTSTATUS = "Chillin";
 
 }

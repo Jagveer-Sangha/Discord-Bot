310a1412d47c914f108dd93c32941210f60307de
Jagveer Sangha <jagveersangha@gmail.com>
2021-06-02 09:49:14 -0400

    Removed unnecessary syntax

--- src/main/java/discord/bot/plug/Invite.java
+++ src/main/java/discord/bot/plug/Invite.java
@@ -5,14 +5,13 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Invite extends ListenerAdapter {
 
-	String url = "";
+	String url = "INVITE SOMEONE TO THIS SERVER USING THIS OR INVITE ME TO A SERVER USING THIS LINK..";
 
 	@Override
 	// Can invite the bot to other servers
 	public void onMessageReceived(MessageReceivedEvent e) {
 		if (e.getMessage().getContentRaw().equals("!invite")) {
 			e.getChannel().sendMessage(String.format(url, e.getJDA().getSelfUser().getId())).queue();
-			;
 		}
 	}
 
